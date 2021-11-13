bf5a242ec70a3993dcaba99760cebdaac0e59ccd
Dennis <izuchukwudennis@gmail.com>
2021-11-13 14:17:24 +0300

    Initial Commit

---
+++ TestTask-QRCodeReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask-QRCodeReader
+//
+//  Created by Izuchukwu Dennis on 13.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
