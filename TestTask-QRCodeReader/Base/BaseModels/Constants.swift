e79c884d4c73e1ca22c64f1b463628bd64a80365
Dennis <izuchukwudennis@gmail.com>
2021-11-17 13:05:45 +0300

    Added the ability to open app settings directly from the app.

--- TestTask-QRCodeReader/Base/BaseModels/Constants.swift
+++ TestTask-QRCodeReader/Base/BaseModels/Constants.swift
@@ -11,6 +11,10 @@ struct Constants{
     static let startScreenLabelText = "Для оплаты заказа отсканйруете QR-код на чеке"
     static let scanButtonTitle = "Сканировать"
     
+    static let alertTitle = "Attention"
+    static let alertMessage = "Please allow access to camera in settings"
+    static let alertActionTitle = "Go to settings"
+    
     static let title = "Оплата счёта"
     static let ScannerVCLabelText = "Наведите камеру на QR-код на чеке"
     
--- TestTask-QRCodeReader/Modules/StartScreenModule/Views/StartScreenViewController.swift
+++ TestTask-QRCodeReader/Modules/StartScreenModule/Views/StartScreenViewController.swift
@@ -6,22 +6,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class StartScreenViewController: UIViewController, Coordinating {
     weak var coordinator: Coordinator?
     let subView = UIView()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         overrideUserInterfaceStyle = .light
         title = Constants.title
         setupUI()
-
+        
+        
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-//        use nslayoutts
         subView.frame = CGRect(x: 0, y: 50, width: view.frame.size.width, height: view.frame.size.height-50)
         scanButton.frame = CGRect(x: 10, y: (7*subView.frame.size.height/8), width: subView.frame.size.width - 20, height: 70)
         displayLabel.frame = CGRect(x: 50, y: (3*subView.frame.size.height/10), width: subView.frame.size.width - 100, height: 200)
@@ -30,7 +30,6 @@ class StartScreenViewController: UIViewController, Coordinating {
     let scanButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
         button.backgroundColor = .red
-//        button.titleLabel?.text = "Сканировать"
         button.layer.cornerRadius = 12
         button.setTitle(Constants.scanButtonTitle, for: .normal)
         button.setTitleColor(.white, for: .normal)
@@ -40,11 +39,15 @@ class StartScreenViewController: UIViewController, Coordinating {
     }()
     
     @objc private func didTapButton(){
+//        check if user granted access to camera
+        guard AVCaptureDevice.authorizationStatus(for: AVMediaType.video) ==  AVAuthorizationStatus.authorized else {
+            handleUserNotAuthorized()
+            return
+        }
         coordinator?.eventOccured(with: .scan)
-        
     }
     
-    let displayLabel:UILabel = {
+    let displayLabel: UILabel = {
         let label = UILabel()
         label.text = Constants.startScreenLabelText
         label.font = .systemFont(ofSize: 20, weight: .bold)
@@ -59,15 +62,42 @@ class StartScreenViewController: UIViewController, Coordinating {
         view.backgroundColor = .red
         subView.backgroundColor = .white
         addSubViews()
-
+        
     }
     private func addSubViews(){
         view.addSubview(subView)
         view.addSubview(scanButton)
         subView.addSubview(displayLabel)
     }
-
-
+    
+    private func presentAlert() {
+        let alert = UIAlertController(title: Constants.alertTitle, message: Constants.alertMessage, preferredStyle: .alert)
+        let action = UIAlertAction(title: Constants.alertActionTitle, style: .default) {[weak self] _ in
+            self?.goToAppSettings()
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func goToAppSettings() {
+        if let url = URL(string: UIApplication.openSettingsURLString) {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+    }
+    
+    private func handleUserNotAuthorized(){
+        AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { [weak self] (granted: Bool) -> Void in
+            DispatchQueue.main.async {
+                guard granted else {
+                    self?.presentAlert()
+                    return
+                }
+                self?.coordinator?.eventOccured(with: .scan)
+            }
+        })
+    }
 
 }
 
